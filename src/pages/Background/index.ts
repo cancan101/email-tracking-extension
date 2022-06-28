c489ab15d59f5d08d7e597d05a0a47e1e7b986fe
Alex Rothberg <agrothberg@gmail.com>
2022-06-28 11:09:39 -0400

    prep for sentry release

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,17 +1,11 @@
 import * as Sentry from '@sentry/react';
 
-import { sentryTunnel, sentryDsn } from '../../settings';
+import { sentrySettings } from '../../settings';
 
 // -------------------------------------------------
 
 Sentry.init({
-  dsn: sentryDsn,
-  tunnel: sentryTunnel,
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  ...sentrySettings,
 });
 
 // -------------------------------------------------
--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,17 @@
+import { BrowserOptions } from '@sentry/browser';
+
 const sentryTunnel = `${process.env.EMAIL_TRACKING_BACKEND_URL}/api/v1/stunnel`;
 const sentryDsn = process.env.SENTRY_DSN;
+const sentryRelease = undefined;
 
-export { sentryTunnel, sentryDsn };
+const sentrySettings: BrowserOptions = {
+  dsn: sentryDsn,
+  tunnel: sentryTunnel,
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  release: sentryRelease,
+};
+
+export { sentrySettings };
