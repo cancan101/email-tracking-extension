94cddd1823706784f58429766fcc5d6a6fb87dbf
Alex Rothberg <agrothberg@gmail.com>
2022-06-28 19:36:34 -0400

    initial usage of material ui

--- src/containers/InboxViewList.tsx
+++ src/containers/InboxViewList.tsx
@@ -2,31 +2,41 @@ import React from 'react';
 import FormatView from './FormatView';
 import { GmailUrlDecoder } from '../utils/GmailUrlDecoder';
 import { View } from '../types';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 
 function InboxViewListItem({
   view,
   closeModal,
+  pos,
 }: {
   view: View;
   closeModal: Function;
+  pos: number;
 }) {
   return (
-    <li
-      className="link-like"
-      onClick={() => {
-        window.location.hash = `#all/${new GmailUrlDecoder().encode(
-          view.tracker.threadId
-        )}`;
-        closeModal();
-      }}
-    >
-      <div className="ellipsis-overflow bolder">
-        {view.tracker.emailSubject || view.tracker.threadId}
-      </div>
-      <div>
-        <FormatView view={view} />
-      </div>
-    </li>
+    <ListItem disablePadding>
+      <ListItemButton
+        onClick={() => {
+          window.location.hash = `#all/${new GmailUrlDecoder().encode(
+            view.tracker.threadId
+          )}`;
+          closeModal();
+        }}
+      >
+        <ListItemText
+          primary={`${pos + 1}. ${
+            view.tracker.emailSubject || view.tracker.threadId
+          }`}
+          primaryTypographyProps={{ noWrap: true, variant: 'subtitle2' }}
+          secondary={<FormatView view={view} />}
+          secondaryTypographyProps={{ variant: 'caption' }}
+          sx={{ marginTop: '0px', marginBottom: '0px' }}
+        />
+      </ListItemButton>
+    </ListItem>
   );
 }
 
@@ -38,10 +48,15 @@ export default function InboxViewList({
   closeModal: Function;
 }) {
   return (
-    <ol>
-      {views.map((view) => (
-        <InboxViewListItem key={view.id} view={view} closeModal={closeModal} />
+    <List dense disablePadding component="ol">
+      {views.map((view, pos) => (
+        <InboxViewListItem
+          key={view.id}
+          view={view}
+          closeModal={closeModal}
+          pos={pos}
+        />
       ))}
-    </ol>
+    </List>
   );
 }
