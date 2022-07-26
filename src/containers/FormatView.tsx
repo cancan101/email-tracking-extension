76b9aea7da5c11b53e60200dd0874303a773c523
Alex Rothberg <agrothberg@gmail.com>
2022-07-26 12:56:00 -0600

    Show email tracker info (#105)

--- src/containers/FormatView.tsx
+++ src/containers/FormatView.tsx
@@ -10,8 +10,14 @@ export default function FormatView({
 }): React.ReactElement {
   let ret = dayjs(view.createdAt).format('L LT');
 
-  if (view.clientIpGeo != null && view.clientIpGeo.data != null) {
-    ret = `${ret} (from: ${view.clientIpGeo.data.city}, ${view.clientIpGeo.data.region})`;
+  const { clientIpGeo } = view;
+
+  if (clientIpGeo != null) {
+    if (clientIpGeo.data != null) {
+      ret = `${ret} (from: ${clientIpGeo.data.city}, ${clientIpGeo.data.region})`;
+    } else if (clientIpGeo.emailProvider != null) {
+      ret = `${ret} (using: ${clientIpGeo.emailProvider})`;
+    }
   }
 
   return <>{ret}</>;
--- src/types.d.ts
+++ src/types.d.ts
@@ -9,6 +9,7 @@ type ClientIpGeo = {
   source: string;
   data?: GeoData;
   rule?: string;
+  emailProvider?: string;
 };
 
 type Tracker = { threadId: string; emailSubject: string };
