1068849d656c9ecc8f73b98d3e73eda24517f12e
Alex Rothberg <agrothberg@gmail.com>
2023-01-11 17:54:59 -0500

    Limit sampling for performance tracing.

--- src/settings.ts
+++ src/settings.ts
@@ -6,14 +6,16 @@ const sentryTunnel = `${process.env.EMAIL_TRACKING_BACKEND_URL}/api/v1/stunnel`;
 const sentryDsn = process.env.SENTRY_DSN;
 const sentryRelease = SENTRY_RELEASE;
 const sentryEnabled = !process.env.SENTRY_DISABLED;
+// Set tracesSampleRate to 1.0 to capture 100%
+// of transactions for performance monitoring.
+const tracesSampleRate = process.env.SENTRY_TRACES_SAMPLE_RATE
+  ? parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE)
+  : 0.1;
 
 const sentrySettings: BrowserOptions = {
   dsn: sentryDsn,
   tunnel: sentryTunnel,
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  tracesSampleRate,
   release: sentryRelease,
 };
 
