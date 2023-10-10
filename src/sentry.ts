b3d852b2af74e55b542c64d6f0adda6d41de6d63
Alex Rothberg <agrothberg@gmail.com>
2023-10-10 14:32:17 -0400

    Fix Sentry and zustand deprecation warnings.

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/react';
-import { BrowserTracing } from '@sentry/tracing';
+import { BrowserTracing } from '@sentry/browser';
 
 import { sentrySettings, sentryEnabled } from './settings';
 
